b1d498f6a74422705794c5b56e4808096d2f6fb8
Nico Stranquist <nstranquist@gmail.com>
2022-01-11 18:45:04 -0600

    pass test

--- farm/farm.js
+++ farm/farm.js
@@ -6,5 +6,6 @@ export class Farm {
   constructor() {
     this.width = INITIAL_WIDTH;
     this.height = INITIAL_HEIGHT;
+    this.mipmap = new Array(INITIAL_WIDTH * INITIAL_HEIGHT).fill(0)
   }
 }
