a33f8c1611fc44aaae03e03f7aaff0060683b483
ericstran <ericstran.stl@gmail.com>
2022-01-16 13:48:05 -0600

    pass first test for helloFarm

--- __tests__/farm.test.js
+++ __tests__/farm.test.js
@@ -4,19 +4,20 @@ import {
 } from '../farm/farm'
 
 // 1. Write Test
-
 // 2. Make sure the test fails
-
 // 3. Write the code to pass the test
-
 // 4. Refactor
 
-describe("text", () => {
-  it("text", () => {
+describe("helloFarm", () => {
+  it("return the greeting message", () => {
     // Arrange
-    
+    const msg = helloFarm()
+
     // Act
 
+
     // Assert
+    expect(msg).toEqual("hello farm")
+  
   })
 })
