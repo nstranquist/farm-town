68fbf33053d3bdb5daf694ad5dabd907d9891937
Nico Stranquist <nstranquist@gmail.com>
2022-01-19 09:45:29 -0600

    try adding gh-pages with travis to deploy app

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
-module.exports = {
-  reactStrictMode: true,
-}
+// Deploy next.js to gh-pages:
+// https://dev.to/jameswallis/deploying-a-next-js-app-to-github-pages-24pn
+
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+module.exports = withPlugins(
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: '/nstranquist/_next/static/images/',
+  }],
+  {
+    reactStrictMode: true,
+    basePath: "/nstranquist/",
+    assetPrefix: "/nstranquist/",
+    env
+})
