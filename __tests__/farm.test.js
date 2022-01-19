e8a0f81b2f4e840f43053b8dfe38766673534aef
Nico Stranquist <nstranquist@gmail.com>
2022-01-18 22:17:53 -0600

    write failing tests for build and clear plots, error cases

--- __tests__/farm.test.js
+++ __tests__/farm.test.js
@@ -10,6 +10,9 @@ import {
 import { mipmapFixtures } from '../farm/fixtures'
 
 describe("Farm Town's Farm", () => {
+  const WIDTH = 5
+  const HEIGHT = 5
+
   describe("Farm Initialization", () => {
     it("generates the farm mipmap", () => {
       const mipmap = INITIAL_MIPMAP();
@@ -61,15 +64,56 @@ describe("Farm Town's Farm", () => {
       const position2 = { x: 1, y: 0 }
       const position3 = { x: 4, y: 3 }
 
-      mipmap = buildPlot(mipmap, 5, 5, position1, 1)
+      mipmap = buildPlot(mipmap, WIDTH, HEIGHT, position1, 1)
       mipmap = expandLeft(mipmap)
-      mipmap = buildPlot(mipmap, 6, 5, position2, 2)
+      mipmap = buildPlot(mipmap, 6, HEIGHT, position2, 2)
       mipmap = expandTop(mipmap)
       mipmap = buildPlot(mipmap, 6, 6, position3, 3)
 
-      console.log(mipmap)
-
       expect(mipmap).toEqual(mipmapFixtures.buildPlot.after)
     })
+  })
+
+  it("throws an appropriate error when position is out of bounds", () => {
+    const mipmap = mipmapFixtures.initial
+
+    expect(() => buildPlot(mipmap, WIDTH, HEIGHT, {x: -1, y: 0}, 1)).toThrow()
+    expect(() => buildPlot(mipmap, WIDTH, HEIGHT, {x: 6, y: 0}, 1)).toThrow()
+    expect(() => buildPlot(mipmap, WIDTH, HEIGHT, {x: 0, y: 5}, 1)).toThrow()
+    expect(() => buildPlot(mipmap, WIDTH, HEIGHT, {x: 0, y: -1}, 1)).toThrow()
+  })
+
+  it("throws an appropriate error when the building value is invalid", () => {
+    const mipmap = mipmapFixtures.initial
+
+    expect(() => buildPlot(mipmap, WIDTH, HEIGHT, {x: 0, y: 0}, -1)).toThrow()
+    expect(() => buildPlot(mipmap, WIDTH, HEIGHT, {x: 0, y: 0}, 0)).toThrow()
+    expect(() => buildPlot(mipmap, WIDTH, HEIGHT, {x: 0, y: 0}, 101)).toThrow()
+  })
+
+  it("resets a specified plot of farm land to its initial state, 0", () => {
+    let mipmap = mipmapFixtures.initial;
+    console.log(mipmap)
+
+    mipmap = buildPlot(mipmap, WIDTH, HEIGHT, {x: 0, y: 0}, 1)
+    console.log(mipmap)
+    expect(mipmap).not.toEqual(mipmapFixtures.initial)
+    
+    mipmap = clearPlot(mipmap, WIDTH, HEIGHT, {x: 0, y: 0})
+    console.log(mipmap)
+
+    expect(mipmap).toEqual(mipmapFixtures.initial)
+  })
+
+  it("throws an error when clearing an invalid plot position or value", () => {
+    const mipmap = mipmapFixtures.initial;
+    console.log(mipmap)
+
+    expect(() => clearPlot(mipmap, WIDTH, HEIGHT, {x: -1, y: 0})).toThrow()
+    expect(() => clearPlot(mipmap, WIDTH, HEIGHT, {x: 5, y: 0})).toThrow()
+    expect(() => clearPlot(mipmap, WIDTH, HEIGHT, {x: 0, y: -1})).toThrow()
+    expect(() => clearPlot(mipmap, WIDTH, HEIGHT, {x: 0, y: 5})).toThrow()
+    // should throw because cannot clear plot with value '0'
+    expect(() => clearPlot(mipmap, WIDTH, HEIGHT, {x: 0, y: 0})).toThrow()
   })
 })
