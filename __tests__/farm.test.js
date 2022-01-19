5db0ac31a7a29d780b1c3df3430a34e3a3e5d2d7
Nico Stranquist <nstranquist@gmail.com>
2022-01-18 21:37:53 -0600

    write test and get it to pass for expandLeft

--- __tests__/farm.test.js
+++ __tests__/farm.test.js
@@ -1,11 +1,17 @@
 import {
-  INITIAL_MIPMAP
+  INITIAL_MIPMAP,
+  expandLeft,
+  expandRight,
+  expandTop,
+  expandBottom,
+  buildPlot,
+  clearPlot
 } from '../farm/farm'
 
 describe("Farm Town's Farm", () => {
   describe("Farm Initialization", () => {
-    it("generates the farm mipmap correctly", () => {
-      let mipmap = INITIAL_MIPMAP();
+    it("generates the farm mipmap", () => {
+      const mipmap = INITIAL_MIPMAP();
 
       console.log(mipmap)
 
@@ -17,5 +23,29 @@ describe("Farm Town's Farm", () => {
         [0, 0, 0, 0, 0],
       ])
     })
+  })
+
+  describe("Farm Expansion", () => {
+    it("expands the farm to the left", () => {
+      const mipmap = [
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+      ]
+
+      const newMipmap = expandLeft(mipmap)
+
+      expect(newMipmap).toEqual([
+        [0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0],
+      ])
+    })
+
+    
   })
 })
