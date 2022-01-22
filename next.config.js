2e15bc0c070d8bd625199ed9d2f35daca9792d9f
Nico Stranquist <nstranquist@gmail.com>
2022-01-21 23:18:52 -0600

    rollback breaking deploy changes

--- next.config.js
+++ next.config.js
@@ -1,24 +1,5 @@
 // Deploy next.js to gh-pages:
-// https://dev.to/jameswallis/deploying-a-next-js-app-to-github-pages-24pn
 
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-
-module.exports = withPlugins(
-  [optimizedImages, {
-    mozjpeg: {
-      quality: 80,
-    },
-    pngquant: {
-      speed: 3,
-      strip: true,
-      verbose: true,
-    },
-    imagesPublicPath: '/nstranquist/_next/static/images/',
-  }],
-  {
+module.exports = {
     reactStrictMode: true,
-    basePath: "/nstranquist/",
-    assetPrefix: "/nstranquist/",
-    env
-})
+}
